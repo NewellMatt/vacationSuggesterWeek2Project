ead1e936bf390458cf83db4a31e99be7ed665fec
Matthew <student@epicodus.com>
2016-01-15 09:47:27 -0800

    Add pictures of Alaska Artic Bahamas and Tahiti and add the display none to the CSS file and add else if clauses to the js file and add a radio form for user input.

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,105 @@
 $(document).ready(function() {
-  $("#celebrityDate").submit(function(event) {
+  $("#vacacationSuggester").submit(function(event) {
     var age = parseInt($("input#age").val());
     var type = $("select#typeOfClimate").val();
+    var hobbieInput = $("input[name=hobbie]:checked").val();
 
     $("#hawaii").hide();
     $("#sm").hide();
     $("#gd").hide();
     $("#lv").hide();
+    $("#at").hide();
+    $("#ak").hide();
+    $("#ba").hide();
+    $("#th").hide();
 
     if (age) {
-      if (type === 'formDropTropical' && age >=50) {
+      if (type === 'formDropTropical' && age >=50 && hobbieInput === "radioValueBeachDay") {
         var oneVariable = "St Martin";
         $("#sm").show();
+        $("#vacaName").text(oneVariable);
+        $("#printToScreen").show();
       }
-      else if (type === 'formDropTropical' && age <50) {
+      else if (type === 'formDropTropical' && age >=50 && hobbieInput === "radioValueAdventure") {
         var oneVariable = "Hawaii";
         $("#hawaii").show();
+        $("#vacaName").text(oneVariable);
+        $("#printToScreen").show();
       }
-      else if (type === 'formDropDry' && age >=50) {
+      else if (type === 'formDropTropical' && age <50 && hobbieInput === "radioValueBeachDay") {
+        var oneVariable = "Hawaii";
+        $("#hawaii").show();
+        $("#vacaName").text(oneVariable);
+        $("#printToScreen").show();
+      }
+      else if (type === 'formDropTropical' && age <50 && hobbieInput === "radioValueSpa") {
+        var oneVariable = "Las Vegas";
+        $("#hawaii").show();
+        $("#vacaName").text(oneVariable);
+        $("#printToScreen").show();
+      }
+      else if (type === 'formDropTropical' && age <50 && hobbieInput === "radioValueAdventure") {
+        var oneVariable = "Hawaii";
+        $("#hawaii").show();
+        $("#vacaName").text(oneVariable);
+        $("#printToScreen").show();
+      }
+      else if (type === 'formDropDry' && age >=50 && hobbieInput === "radioValueSpa") {
         var oneVariable = "Las Vegas";
         $("#lv").show();
+        $("#vacaName").text(oneVariable);
+        $("#printToScreen").show();
       }
-      else if (type === 'formDropDry' && age <50) {
+      else if (type === 'formDropDry' && age >=50 && hobbieInput === "radioValueAdventure") {
+        var oneVariable = "Las Vegas";
+        $("#lv").show();
+        $("#vacaName").text(oneVariable);
+        $("#printToScreen").show();
+      }
+      else if (type === 'formDropDry' && age <50 && hobbieInput === "radioValueAdventure") {
         var oneVariable = "Gobi Desert";
         $("#gd").show();
-
-
-      $("#vacaName").text(oneVariable);
-      $("#printToScreen").show();
+        $("#vacaName").text(oneVariable);
+        $("#printToScreen").show();
+      }
+      else if (type === 'formDropMild' && age >=50 && hobbieInput === "radioValueBeachDay") {
+        var oneVariable = "Bahamas";
+        $("#ba").show();
+        $("#vacaName").text(oneVariable);
+        $("#printToScreen").show();
+      }
+      else if (type === 'formDropMild' && age >=50 && hobbieInput === "radioValueAdventure") {
+        var oneVariable = "Bahamas";
+        $("#ba").show();
+        $("#vacaName").text(oneVariable);
+        $("#printToScreen").show();
+      }
+      else if (type === 'formDropMild' && age <50 && hobbieInput === "radioValueBeachDay") {
+        var oneVariable = "Hawaii";
+        $("#hawaii").show();
+        $("#vacaName").text(oneVariable);
+        $("#printToScreen").show();
+      }
+      else if (type === 'formDropMild' && age <50 && hobbieInput === "radioValueAdventure") {
+        var oneVariable = "Hawaii";
+        $("#hawaii").show();
+        $("#vacaName").text(oneVariable);
+        $("#printToScreen").show();
+      }
+      else if (type === 'formDropPolar' && age >=50 && hobbieInput === "radioValueAdventure") {
+        var oneVariable = "Alaska";
+        $("#ak").show();
+        $("#vacaName").text(oneVariable);
+        $("#printToScreen").show();
+      }
+      else if (type === 'formDropPolar' && age <50 && hobbieInput === "radioValueAdventure") {
+        var oneVariable = "Artic";
+        $("#at").show();
+        $("#vacaName").text(oneVariable);
+        $("#printToScreen").show();
       }
       else {
-          alert('Please enter your age!');
+          alert('Please Finish All Answers');
         }
       event.preventDefault();
     };
