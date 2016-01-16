94a4f73da0189d554d3395b14ddee92809d9893e
Matthew <student@epicodus.com>
2016-01-15 16:35:19 -0800

    Delete the CheckBox, Add functionality to Favorite Number

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     var age = parseInt($("input#age").val());
     var type = parseInt($("select#typeOfClimate").val());
     var hobbieInput = parseInt($("input[name=hobbie]:checked").val());
-    var nameInput = $("input#nameID").val();
+    var nameInput = parseInt($("input#favoriteNumberID").val());
     var typeOfLocation = parseInt($("select#typeOfLocation").val());
 
     $("#hawaii").hide();
@@ -19,19 +19,19 @@ $(document).ready(function() {
 
 
     if (age) {
-        if ((type+age+hobbieInput+typeOfLocation > 0 && type+age+hobbieInput+typeOfLocation <= 7) || (type+age+hobbieInput+typeOfLocation > 28 && type+age+hobbieInput+typeOfLocation <= 35)) {
+        if ((nameInput+type+age+hobbieInput+typeOfLocation > 0 && nameInput+type+age+hobbieInput+typeOfLocation <= 7) || (nameInput+type+age+hobbieInput+typeOfLocation > 28 && nameInput+type+age+hobbieInput+typeOfLocation <= 35)) {
         var oneVariable = "Bend, Oregon";
         $("#bo").show();
         $("#vacaName").text(oneVariable);
         $("#printToScreen").show();
       }
-      else if ((type+age+hobbieInput+typeOfLocation > 7 && type+age+hobbieInput+typeOfLocation <= 14) || (type+age+hobbieInput+typeOfLocation > 35)) {
+      else if ((nameInput+type+age+hobbieInput+typeOfLocation > 7 && nameInput+type+age+hobbieInput+typeOfLocation <= 14) || (nameInput+type+age+hobbieInput+typeOfLocation > 35)) {
         var oneVariable = "Hawaii";
         $("#hawaii").show();
         $("#vacaName").text(oneVariable);
         $("#printToScreen").show();
       }
-      else if ((type+age+hobbieInput+typeOfLocation > 14 && type+age+hobbieInput+typeOfLocation <= 28) || (type+age+hobbieInput+typeOfLocation > 35)) {
+      else if ((nameInput+type+age+hobbieInput+typeOfLocation > 14 && nameInput+type+age+hobbieInput+typeOfLocation <= 28) || (nameInput+type+age+hobbieInput+typeOfLocation > 35)) {
         var oneVariable = "St Martin";
         $("#sm").show();
         $("#vacaName").text(oneVariable);
